f274697caa94f7a3eeacfd6601c74daa211b49c9
Vladimir Bondar <vdecbel@gmail.com>
2025-04-02 10:48:15 +0300

    cup of join

--- src/5.2.cup_of_join.py
+++ src/5.2.cup_of_join.py
@@ -2,22 +2,22 @@
 This script defines a function that joins multiple lists with an optional separator.
 """
 
-def cup_of_join(*lists, sep=None):
+def cup_of_join(*lists, sep = None):
     """
-    Joins multiple lists into one, optionally inserting a separator between them.
+        Joins multiple lists into one, optionally inserting a separator between them.
 
-    :param lists: Lists to be joined.
-    :param sep: Separator to be inserted between lists (default: None).
-    :return: The joined list or None if the result is empty.
-    """
+        :param lists: Lists to be joined.
+        :param sep: Separator to be inserted between lists (default: None).
+        :return: The joined list or None if the result is empty.
+        """
     result = []
 
-    for lst in lists:  # Append lists including the separator
+    for lst in lists:  #append lists including the separator in the beginning
         result += lst
         if sep is not None:
             result.append(sep)
 
-    return result[:-1] if result else None  # Remove the last separator if present
+    return result if len(result) !=0 else None  #return not including the first separator or None if it's empty
 
 if __name__ == '__main__':
     print(cup_of_join([1, 2], [8], [9, 5, 6]))
